e0b8031e49fd3b388f70016cf66cf67952f3b6e5
NjugunaW <njugunawangui10@gmail.com>
2024-02-19 22:30:21 +0300

    BodySection Correction

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,42 +1,42 @@
-import { shallow, mount } from "enzyme";
-import React from "react";
-import BodySectionWithMarginBottom from "./BodySectionWithMarginBottom";
-import { StyleSheetTestUtils } from "aphrodite";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import BodySection from "./BodySection";
+import { StyleSheet, css } from "aphrodite";
 
-describe("<BodySectionWithMarginBottom />", () => {
-  beforeAll(() => {
-    StyleSheetTestUtils.suppressStyleInjection();
-  });
-  afterAll(() => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-  });
+class BodySectionWithMarginBottom extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  it("BodySectionWithMarginBottom renders without crashing", () => {
-    const wrapper = shallow(<BodySectionWithMarginBottom />);
-    expect(wrapper.exists()).toEqual(true);
-  });
-
-  it("Shallowing the component should render correctly a BodySection component and that the props are passed correctly to the child component", () => {
-    const wrapper = shallow(
-      <BodySectionWithMarginBottom title="test title">
-        <p>test children node</p>
-      </BodySectionWithMarginBottom>
+  render() {
+    return (
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection {...this.props} />
+      </div>
     );
+  }
+}
 
-    const BodySection = wrapper.find("BodySection");
+BodySectionWithMarginBottom.defaultProps = {
+  title: "",
+};
 
-    expect(BodySection).toHaveLength(1);
-    expect(BodySection.props().title).toEqual("test title");
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string,
+};
 
-    const internalBody = BodySection.dive();
+const screenSize = {
+  small: "@media screen and (max-width: 900px)",
+};
 
-    const h2 = internalBody.find("h2");
-    const p = internalBody.find("p");
-
-    expect(h2).toHaveLength(1);
-    expect(h2.text()).toEqual("test title");
-
-    expect(p).toHaveLength(1);
-    expect(p.text()).toEqual("test children node");
-  });
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: "40px",
+    width: "100%",
+    [screenSize.small]: {
+      marginBottom: "10px",
+    },
+  },
 });
+
+export default BodySectionWithMarginBottom;
